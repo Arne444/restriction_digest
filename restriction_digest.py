606af0b00eaf12ae41ab78fd5f0625adb201d360
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-27 10:50:35 +0100

    Edit code WORKS

--- restriction_digest.py
+++ restriction_digest.py
@@ -3,23 +3,23 @@
 from opentrons import containers, robot, instruments
 
 # specify containers
-output = containers.load('96-PCR-flat', 'C1', 'output')
+output = containers.load('96-PCR-flat', 'C2', 'output')
 trash = containers.load('trash-box', 'A3')
-source_tubes = containers.load('tube-rack-2ml', 'D1', 'tube_rack')
-cold_tubes = containers.load('tube-rack-2ml', 'E5', 'cold_rack')
+source_tubes = containers.load('tube-rack-2ml', 'C1', 'tube_rack')
+cold_tubes = containers.load('tube-rack-2ml', 'E3', 'cold_rack')
 
 p200rack = containers.load('tiprack-200ul', 'A1', 'p200_rack')
 
-#Create 6x12 p20 tip rack
+#Create 5x12 p20 tip rack
 containers.create(
-	'tiprack-200ul-6x12',
-	grid=(6,12),
+	'tiprack-200ul-5x12',
+	grid=(5,12),
 	spacing=(9, 9),
 	diameter=5,
 	depth=60
 )
 
-p20rack = containers.load('tiprack-200ul-6x12', 'B2', 'p20_rack')
+p20rack = containers.load('tiprack-200ul-5x12', 'E1', 'p20_rack')
 
 #Create 3x6 2ml tube rack for DNA samples
 containers.create(
@@ -35,7 +35,7 @@ DNA_tubes = containers.load('3x6-tube-rack-2ml', 'C3', 'DNA_rack')
 #Define pipettes
 p20 = instruments.Pipette(
 	trash_container=trash,
-	tip_rack=[p20rack],
+	tip_racks=[p20rack],
 	min_volume=2,
 	max_volume=20,
 	axis="a"
@@ -43,7 +43,7 @@ p20 = instruments.Pipette(
 
 p200 = instruments.Pipette(
 	trash_container=trash,
-	tip_rack=[p200rack],
+	tip_racks=[p200rack],
 	min_volume=20,
 	max_volume=200,
 	axis="b"
@@ -76,7 +76,7 @@ DNA_sources = DNA_tubes.wells('A1', length=num_DNA_samples)
 p20.transfer(
 	buffer_volume,
 	buffer_source,
-	output.wells('A1', length(num_DNA_samples)),
+	output.wells('A1', length=(num_DNA_samples)),
 	touch_tip=True,
 	blow_out=True
 )
@@ -85,8 +85,8 @@ p20.transfer(
 p20.transfer(
 	enzymeA_volume,
 	enzymeA_source,
-	output.wells('A1', length(num_DNA_samples)),
-	mix_after=(3, 20),
+	output.wells('A1', length=(num_DNA_samples)),
+	mix_after=(3, 10),
 	touch_tip=True,
 	blow_out=True,
 	new_tip='always'
@@ -96,8 +96,8 @@ p20.transfer(
 p20.transfer(
 	enzymeB_volume,
 	enzymeB_source,
-	output.wells('A1', length(num_DNA_samples)),
-	mix_after=(3, 20),
+	output.wells('A1', length=(num_DNA_samples)),
+	mix_after=(3, 10),
 	touch_tip=True,
 	blow_out=True,
 	new_tip='always'
@@ -107,8 +107,8 @@ p20.transfer(
 p20.transfer(
 	DNA_volumes,
 	DNA_sources,
-	output.wells('A1', length(num_DNA_samples)),
-	mix_after=(3, 20),
+	output.wells('A1', length=(num_DNA_samples)),
+	mix_after=(3, 10),
 	touch_tip=True,
 	blow_out=True,
 	new_tip='always'
@@ -118,7 +118,7 @@ p20.transfer(
 p200.transfer(
 	water_volumes,
 	water_source,
-	output.wells('A1', length(num_DNA_samples)),
+	output.wells('A1', length=(num_DNA_samples)),
 	mix_after=(5, 48),
 	touch_tip=True,
 	blow_out=True,
